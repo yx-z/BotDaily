a15876a13cc76ea1f0ede83515b7c7c8cd5073fc
David Zhang <yx-z@outlook.com>
2020-11-27 14:27:30 -0500

    external text
    Signed-off-by: David Zhang <yx-z@outlook.com>

--- feature/external_text.py
+++ feature/external_text.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from feature.text import Text
 from utility.system import get_resource_path
+from utility.constant import *
 
 
 class ExternalText(Text):
@@ -37,6 +38,17 @@ class ExternalText(Text):
 # html_emphasis(text: str, bold: bool = True, italic: bool = True)
 # html_a(text: str, url: str, style: str = "", **kwargs)
 # css_build(*styles: str, **kw_styles)
+# HTML_NEW_LINE = "<br>"
+# HTML_LESS_THAN_TEXT = "&lt;"
+# HTML_GREATER_THAN_TEXT = "&gt;"
+
+# CSS_DEFAULT_DIV = "font-size: 1.15em; color: #000; padding: 5px;"
+# CSS_FULL_WIDTH = "width: 100%;"
+# CSS_CENTER = "text-align: center;"
+# CSS_BIG = "display: block; width: 90%; margin: auto;"
+# CSS_MEDIUM = "display: block; width: 60%; margin: auto;"
+# CSS_SMALL = "display: block; width: 40%; margin: auto;"
+
 f"""
 
 """
