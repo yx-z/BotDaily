b980ba07832fdcd8e94203011d664645c7c36c4c
yx-z <yx-z@outlook.com>
2020-12-20 13:13:14 -0500

    fix css

--- utility/constant.py
+++ utility/constant.py
@@ -12,7 +12,7 @@ HTML_SINGLE_QUOTE = "&#39;"
 
 CSS_DEFAULT_DIV = "font-size: 1.15em; color: #000; padding: 5px;"
 CSS_FULL_WIDTH = "width: 100%;"
-CSS_CENTER = "txt-align: center;"
+CSS_CENTER = "text-align: center;"
 CSS_BIG = "display: block; width: 90%; margin: auto;"
 CSS_MEDIUM = "display: block; width: 60%; margin: auto;"
 CSS_SMALL = "display: block; width: 40%; margin: auto;"
